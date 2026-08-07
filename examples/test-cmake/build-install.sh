a022982484ce73bea0c232476665787fa98a39e7
Daniel Bevenius <daniel.bevenius@gmail.com>
2026-08-07 16:45:35 +0200

    cmake : add config version support (wip) [no ci]
    This commit adds support for find_package using a version, for example:
    ```
    find_package(ggml 0.19.0 REQUIRED)
    ```
    
    examples/test-cmake has been updated to use this and build scripts have
    been added to verify this manually. This is still a work in progress and
    I'm not sure about the scripts and if we can find better ways to test
    this but it might be useful to have for verification of changes to the
    cmake build.

---
+++ examples/test-cmake/build-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+# Remove installation target directory
+rm -rf install-dl install ggml-build-install-dl ggml-build-install
+
+build_dir=ggml-build-install-dl
+install_dir=install-dl
+cmake --fresh -S ../../. -B $build_dir -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=ON \
+  -DGGML_BACKEND_DL=ON \
+  -DGGML_CPU_ALL_VARIANTS=ON \
+  -DCMAKE_INSTALL_PREFIX="${PWD}/${install_dir}" \
+  -DGGML_BACKEND_DIR="${PWD}/${install_dir}/lib/ggml"
+
+cmake --build $build_dir --parallel 12
+cmake --install $build_dir
+
+build_dir=ggml-build-install
+install_dir=install
+cmake --fresh -S ../../. -B $build_dir -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_INSTALL_PREFIX="${PWD}/${install_dir}"
+
+cmake --build $build_dir --parallel 12
+cmake --install $build_dir
